73b63e0b2632589a18765d21abb665b536aedbbe
TailWisteria <87256934+TailWisteria@users.noreply.github.com>
2021-07-28 17:59:14 +0900

    ユーザー新規登録のテストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :cc_last_name, presence: true
-  validates :cc_first_name, presence: true
-  validates :kk_last_name, presence: true
-  validates :kk_first_name, presence: true
+  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :cc_last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :cc_first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :kk_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :kk_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
-  
+
 end
