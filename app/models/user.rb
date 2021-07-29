5ad901ac57f41f0b51249df58cb4645a11c05411
TailWisteria <87256934+TailWisteria@users.noreply.github.com>
2021-07-29 09:42:08 +0900

    修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :password, format: { with: /\A[a-zA-Z0-9]+\z/ } 
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i } 
 
   with_options presence: true do
 
