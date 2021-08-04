62799a126e1a6c5f12c8dcebde08a6c3eec61bef
TailWisteria <87256934+TailWisteria@users.noreply.github.com>
2021-08-04 09:39:33 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,9 +19,15 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    if current_user.id = Item.find(params[:item_id]).user_id || Item.find(params[:item_id]).order.present?
+
+    if current_user.id == Item.find(params[:item_id]).user_id
       redirect_to root_path
     end
+
+    if Item.find(params[:item_id]).order.present?
+       redirect_to root_path
+    end
+
   end
 
   private
